2eab326b39621d4eacf7e0b6be60eb08d37f12f9
jloudenslager <loudenslager.15@osu.edu>
2021-02-02 20:41:08 -0500

    Pretend to login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from './Dashboard';
 import Login from './Login'
 
 function App() {
   return (
     <div>
       <h1>Nursing Doctoral Student Dashboard</h1>
-      <Login></Login>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard></Dashboard>
+          </Route>
+          <Route exact path="/">
+            <Login></Login>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import './Login.css';
 
 export default class Login extends Component {
     constructor(props) {
         super(props);
-        this.state = {username: "", password: ""};
+        this.state = {
+            username: "",
+            password: "",
+            loggedIn: false
+        };
 
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -23,10 +28,15 @@ export default class Login extends Component {
 
     handleSubmit(e) {
         //TODO: Do something with the submitted username and password
+        this.setState({loggedIn: true});
         e.preventDefault();
     }
     
     render() {
+        if (this.state.loggedIn) {
+            return <Redirect to="/dashboard" />
+        }
+
         return (
             <div id="login-wrapper">
                 <form onSubmit={this.handleSubmit}>
