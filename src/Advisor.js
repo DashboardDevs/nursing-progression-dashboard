92e73f93ccac4febd10fde8f0dd6a4d81594a529
Kyle Schaffer <kyle.schaffer@gmail.com>
2021-02-22 16:02:56 -0500

    Initial Advisor View

---
+++ src/Advisor.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Checklist from './Checklist';
+
+export default class Advisor extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            students: []
+        }
+    }
+
+    componentDidMount() {
+        //This will be an api call to the mock data server later
+        const url = "./Data/MOCK_DATA.json"
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                var visibleStudents = data.slice(0, 10); //Only store first 10 students in the state
+                this.setState({ students: visibleStudents });
+                console.log(this.state.students);
+            })
+    }
+
+    render() {
+        return (
+            <div class="m-5">
+                {this.state.students.map(student =>
+                    <div class="bg-white border-2 p-6 rounded-md shadow-lg">
+                        <h2 class="text-3xl">{student.first_name} {student.last_name}</h2>
+                        <Checklist />
+                    </div>
+                )}
+            </div>
+        )
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+  
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Advisor from './Advisor';
+
 function App() {
   return (
-    <div>
-      <h1>Nursing Doctoral Student Dashboard</h1>      
-    </div>
+    <Router>
+      <nav>
+        <ul class="border-0 border-b-2">
+          <li class="text-center">
+            <Link class="text-scarlet underline" to="/advisor">Advisor</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+      <Route path="/advisor">
+          <div>
+            <Advisor />
+          </div>
+        </Route>
+        <Route path="/">
+          <div>
+            <h1>Nursing Doctoral Student Dashboard</h1>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
