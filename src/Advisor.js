a59e99452a41af41ce3b395d01f6cc2fff1e683d
TylerO <olgeatytyler@gmail.com>
2021-03-04 02:56:21 -0500

    Search bar functionality

--- src/Advisor.js
+++ src/Advisor.js
@@ -7,9 +7,15 @@ export default class Advisor extends Component {
 
     constructor(props) {
         super(props);
+        this.handleSearchChange = this.handleSearchChange.bind(this);
         this.state = {
-            students: []
+            students: [],
+            filterText: ''
         }
+    }
+
+    handleSearchChange(text){
+        this.setState({filterText: text});
     }
 
     componentDidMount() {
@@ -18,23 +24,24 @@ export default class Advisor extends Component {
         fetch(url)
             .then(res => res.json())
             .then(data => {
-                var visibleStudents = data.slice(0, 10); //Only store first 10 students in the state
+                var visibleStudents = data;
                 this.setState({ students: visibleStudents });
                 console.log(this.state.students);
             })
     }
 
     render() {
+        const text = this.state.filterText;
         return (
             <div class="flex justify-center">
                 <div class="flex flex-col mx-2 w-3/5">
                     <div class="grid grid-cols-2">
                         <div class="text-xl font-semibold">Student Milestone Dashboard</div>
                         <div class="justify-self-end bg-gray-200 rounded-full my-1 px-16 text-gray-400">
-                            <SearchBar/>
+                            <SearchBar filterText={text} onTextChange={this.handleSearchChange}/>
                         </div>
                     </div>
-                    <StudentTable students={this.state.students}/>
+                    <StudentTable searched={this.state.filterText} students={this.state.students}/>
                 </div>
             </div>
         )
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
 
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.props.onTextChange(e.target.value);
+    }
+
     render(){
+        const searchText = this.props.filterText;
         return(
             <div>
-                Search... 
+                <input class="bg-transparent text-center w-full" type="text" value={searchText} onChange = {this.handleChange} placeholder='Search a Student'/>
                 {/* Placeholder Search Function */}
             </div>
         )
