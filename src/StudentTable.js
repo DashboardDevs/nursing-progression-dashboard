5b25176ba11be327e1c758fbd9df9f8b4c7c46aa
TylerO <olgeatytyler@gmail.com>
2021-03-05 04:22:27 -0500

    Added expandable milestone containers

--- src/StudentTable.js
+++ src/StudentTable.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MilestoneContainer from './MilestoneContainer';
 
 export default class StudentTable extends Component {
 
@@ -19,7 +20,7 @@ export default class StudentTable extends Component {
             <div>
                 <div class="flex flex-col justify-center items-center rounded-lg bg-gray-200 border mb-4 p-4">
                         {showNames.map(name => (
-                            <div class="flex flex-col border mb-2 w-full p-2 h-48 bg-white rounded-lg">
+                            <div class="flex flex-col border mb-2 w-full p-2 bg-white rounded-lg">
                                 <div class="flex items-center">
                                     <div class="bg-gray-200 w-36 text-center font-semibold py-1 rounded-lg">{name}</div>
                                     <div class=" bg-gray-200 mx-2 h-4 w-full rounded-full">
@@ -27,6 +28,7 @@ export default class StudentTable extends Component {
                                         <div class="bg-red-300 h-full w-2/5 rounded-full"></div>
                                     </div>
                                 </div>
+                                <MilestoneContainer/>
                             </div>
                         ))}
                 </div>
