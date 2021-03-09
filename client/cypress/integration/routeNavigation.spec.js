caf3b4429976d5184df0f1780cd5f7b18a5ed870
jloudenslager <loudenslager.15@osu.edu>
2021-03-09 17:40:53 -0500

    Expanded tests

--- client/cypress/integration/routeNavigation.spec.js
+++ client/cypress/integration/routeNavigation.spec.js
@@ -1,19 +1,31 @@
 describe("Routes", () => {
-    before(() => {
-      cy.visit("/");
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("Redirects to Student page from Login page", () => {
+    cy.get(".text-center > a")
+      .eq(1)
+      .click();
+    cy.location("pathname", { timeout: 60000 }).should("include", "/student");
+  });
+
+  it("Redirects to Advisor page from Login page", () => {
+      cy.get(".text-center > a")
+        .eq(0)
+        .click();
+      cy.location("pathname", { timeout: 60000 }).should("include", "/advisor");
     });
-  
-    it("Redirects to Student page from Login page", () => {
+
+    it("Redirects to Update page from Student Page", () => {
       cy.get(".text-center > a")
         .eq(1)
         .click();
-      cy.location("pathname", { timeout: 60000 }).should("include", "/student");
-    });
 
-    it("Redirects to Advisor page from Login page", () => {
-        cy.get(".text-center > a")
-          .eq(0)
-          .click();
-        cy.location("pathname", { timeout: 60000 }).should("include", "/advisor");
-      });
-  });
+      cy.get("a[href='/update']")
+        .eq(0)
+        .click();
+
+      cy.location("pathname", { timeout: 60000 }).should("include", "/update");
+    });
+});
