2fc4dc950e6a34408a143d5424178122d9f48d36
rachelbobango <54557567+rachelbobango@users.noreply.github.com>
2021-03-29 20:54:36 -0400

    Small student view UI changes

--- client/src/Student.js
+++ client/src/Student.js
@@ -37,6 +37,9 @@ export default class Student extends Component {
             })
     }
 
+    //                <Link class="bg-scarlet text-white py-2 px-6 rounded-3xl h-1/2 mt-5" to={{pathname: formLink, student:this.state.student}}>Update Milestones</Link>
+
+
     // Currently uses the current User's name instead of the student's name.
     // need to add id to student object and do a /Student/:id route to pull the right data
     render() {
@@ -72,47 +75,46 @@ export default class Student extends Component {
         }
 
         return (
-            <div>
-                <div class="w-full flex space-x-2">
-                    <h1 class="w-10/12 text-scarlet m-5 text-4xl">{this.state.student.last_name}, {this.state.student.first_name}</h1>
-                    <Link class="bg-scarlet text-white py-2 px-6 rounded-3xl h-1/2 mt-5" to={{pathname: formLink, student:this.state.student}}>Update Milestones</Link>
+            <div class="mt-8">
+                <div class="flex items-center ml-5 mr-5">
+                        <h1 class="bg-gray-200 w-2/6 text-center font-semibold py-1 rounded-lg text-4xl">{this.state.student.last_name}, {this.state.student.first_name}</h1>
+                        <div class=" bg-gray-200 mx-2 h-4 w-full rounded-full" >
+                            <div class="bg-green-500 h-full rounded-full" style={{width: setWidth}}></div>
+                    </div>
                 </div>
-                <p id="gradDate" class="w-10/12 text-black text-xl">Expected Graduation Date: {this.state.student.graduation_date.slice(5,7)}/{this.state.student.graduation_date.slice(8,10)}/{this.state.student.graduation_date.slice(0,4)}</p>
-                <div id="progressBar" >
-                    <div id="bar" style={{width: setWidth}}></div>
+                <div class="flex">
+                    <p id="gradDate" class="bg-gray-200 w-2/6 text-left font-semibold py-1 rounded-lg text-xl ml-5 mt-3">Expected Graduation Date: {this.state.student.graduation_date.slice(5,7)}/{this.state.student.graduation_date.slice(8,10)}/{this.state.student.graduation_date.slice(0,4)}</p>
+                    <div class="w-4/6">
+                        <Link class="float-right bg-scarlet text-white py-2 px-6 rounded-3xl h-2/3 mt-5 mr-6" to={{pathname: formLink, student:this.state.student}}>Update Milestones</Link>
+                    </div>
                 </div>
-                <div>
-                    <h2 class="pt-5 w-10/12 text-black m-5 text-4xl">
-                        Milestones
-                    </h2>
+                <div class="ml-5 mr-5 mt-3 p-5 bg-gray-200 rounded-lg">
+                    <CollapsibleComponent class="m-3  p-5 text-black font-bold text-center text-base bg-white grid grid-cols-4 gap-3">
+                        {sortedMilestones.map((milestone) => {
+                            let color = ""
+                            if(milestone.status === 0) {
+                                color = "bg-red-600";
+                            } else if(milestone.status === 1) {
+                                color = "bg-yellow-500";
+                            } else if(milestone.status === 2) {
+                                color = "bg-gray-500";
+                            } else if(milestone.status === 3) {
+                                color = "bg-green-500";
+                            }
+                            return (
+                                <div>
+                                    <CollapsibleHead class="pb-5 pl-5 bg-gray-400 rounded-lg">
+                                        <div className={`my-3.5 circle ${color} align-middle float-left`}></div>
+                                        <div class="w-10/12 float-right">{milestone.name}</div>
+                                    </CollapsibleHead>
+                                    <CollapsibleContent class="bg-gray-400 bg-opacity-30">
+                                        <p>{milestone.description}</p>
+                                    </CollapsibleContent>
+                                </div>
+                            )
+                        })}
+                    </CollapsibleComponent>
                 </div>
-                <CollapsibleComponent class="m-5  p-5 text-black font-bold text-center text-lg bg-gray-400 bg-opacity-30 grid grid-cols-4 gap-5">
-                    {sortedMilestones.map((milestone) => {
-                        let color = ""
-                        if(milestone.status === 0) {
-                            color = "bg-red-600";
-                        } else if(milestone.status === 1) {
-                            color = "bg-yellow-500";
-                        } else if(milestone.status === 2) {
-                            color = "bg-gray-500";
-                        } else if(milestone.status === 3) {
-                            color = "bg-green-500";
-                        }
-
-                        return (
-                            <div>
-                                <CollapsibleHead class="pb-5 pl-5 bg-white rounded-lg">
-                                    <div className={`my-3.5 circle ${color} align-middle float-left`}></div>
-                                    <div class="w-10/12 float-right">{milestone.name}</div>
-                                </CollapsibleHead>
-                                <CollapsibleContent class="bg-gray-400 bg-opacity-30">
-                                    <p>{milestone.description}</p>
-                                </CollapsibleContent>
-                            </div>
-                        )
-                    })}
-                </CollapsibleComponent>
-                
             </div>
         );
     }
