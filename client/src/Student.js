44edbc4eb7b715726e3337cb001e34cc8cdefd45
alejandra-martinez <alejandrac.cmartinez@gmail.com>
2021-04-01 16:49:10 -0400

    display notes on advisor view of students

--- client/src/Student.js
+++ client/src/Student.js
@@ -7,22 +7,22 @@ import './Student.css';
 import CollapsibleComponent from './CollapsibleComponent';
 import CollapsibleHead from './CollapsibleHead';
 import CollapsibleContent from './CollapsibleContent';
+import AdvisorNotesComponent from './AdvisorNotesComponent';
 
 export default class Student extends Component { 
 
     constructor(props) {
         super(props);
-        this.state = {isLoading: true};
-        console.log(props);
+        let path = window.location.pathname.split("/");
+        let id = path[path.length - 1];
+        this.state = {isLoading: true, id: id};
     }
 
 
     componentDidMount() {
-        let path = window.location.pathname.split("/");
-        let id = path[path.length - 1];
-        console.log(id);
+        console.log(this.state.id);
 
-        const urlStudent = `http://localhost:3001/student/${id}`;
+        const urlStudent = `http://localhost:3001/student/${this.state.id}`;
         fetch(urlStudent)
             .then(res => res.json())
             .then(student => {
@@ -31,8 +31,8 @@ export default class Student extends Component {
                     .then(res => res.json())
                     .then(milestones => {
                         this.setState({ student: student[0], milestones: milestones, isLoading: false });
-                        console.log(this.state.student);
-                        console.log(this.state.milestones);
+                        //console.log(this.state.student);
+                        //console.log(this.state.milestones);
                     })
             })
     }
@@ -54,65 +54,110 @@ export default class Student extends Component {
         let count = 0;
 
         sortedMilestones.forEach((milestone) => {
-            console.log(milestone);
+            //console.log(milestone);
             if(milestone.status===3){
                 count+=1;
             }
         });
 
-        console.log("count", count);
+        //console.log("count", count);
         let width = (count/14) * 100;
         let setWidth = width +"%";
         //Set the link to the form to the update form for advisors or the request form for students
         let formLink;
-        if(this.props.currentUser.perms === 1){
-            formLink = "/update";
-        }else{
-            formLink = "/request";
-        }
-
-        return (
-            <div>
-                <div class="w-full flex space-x-2">
-                    <h1 class="w-10/12 text-scarlet m-5 text-4xl">{this.state.student.last_name}, {this.state.student.first_name}</h1>
-                    <Link class="bg-scarlet text-white py-2 px-6 rounded-3xl h-1/2 mt-5" to={{pathname: formLink, student:this.state.student}}>Update Milestones</Link>
-                </div>
-                <div id="progressBar" >
-                    <div id="bar" style={{width: setWidth}}></div>
-                </div>
+        
+        if(this.props.currentUser.perms==0){
+            return (
                 <div>
-                    <h2 class="pt-5 w-10/12 text-black m-5 text-4xl">
-                        Milestones
-                    </h2>
-                </div>
-                <CollapsibleComponent class="m-5  p-5 text-black font-bold text-center text-lg bg-gray-400 bg-opacity-30 grid grid-cols-4 gap-5">
-                    {sortedMilestones.map((milestone) => {
-                        let color = ""
-                        if(milestone.status === 0) {
-                            color = "bg-red-600";
-                        } else if(milestone.status === 1) {
-                            color = "bg-yellow-500";
-                        } else if(milestone.status === 2) {
-                            color = "bg-gray-500";
-                        } else if(milestone.status === 3) {
-                            color = "bg-green-500";
-                        }
+                    <div class="w-full flex space-x-2">
+                        <h1 class="w-10/12 text-scarlet m-5 text-4xl">{this.state.student.last_name}, {this.state.student.first_name}</h1>
+                        <Link class="bg-scarlet text-white py-2 px-6 rounded-3xl h-1/2 mt-5" to={{pathname: "/request", student:this.state.student}}>Update Milestones</Link>
+                    </div>
+                    <div id="progressBar" >
+                        <div id="bar" style={{width: setWidth}}></div>
+                    </div>
+                    <div>
+                        <h2 class="pt-5 w-10/12 text-black m-5 text-4xl">
+                            Milestones
+                        </h2>
+                    </div>
+                    <CollapsibleComponent class="m-5  p-5 text-black font-bold text-center text-lg bg-gray-400 bg-opacity-30 grid grid-cols-4 gap-5">
+                        {sortedMilestones.map((milestone) => {
+                            let color = ""
+                            if(milestone.status === 0) {
+                                color = "bg-red-600";
+                            } else if(milestone.status === 1) {
+                                color = "bg-yellow-500";
+                            } else if(milestone.status === 2) {
+                                color = "bg-gray-500";
+                            } else if(milestone.status === 3) {
+                                color = "bg-green-500";
+                            }
 
-                        return (
-                            <div>
-                                <CollapsibleHead class="pb-5 pl-5 bg-white rounded-lg">
-                                    <div className={`my-3.5 circle ${color} align-middle float-left`}></div>
-                                    <div class="w-10/12 float-right">{milestone.name}</div>
-                                </CollapsibleHead>
-                                <CollapsibleContent class="bg-gray-400 bg-opacity-30">
-                                    <p>{milestone.description}</p>
-                                </CollapsibleContent>
-                            </div>
-                        )
-                    })}
-                </CollapsibleComponent>
+                            return (
+                                <div>
+                                    <CollapsibleHead class="pb-5 pl-5 bg-white rounded-lg">
+                                        <div className={`my-3.5 circle ${color} align-middle float-left`}></div>
+                                        <div class="w-10/12 float-right">{milestone.name}</div>
+                                    </CollapsibleHead>
+                                    <CollapsibleContent class="bg-gray-400 bg-opacity-30">
+                                        <p>{milestone.description}</p>
+                                    </CollapsibleContent>
+                                </div>
+                            )
+                        })}
+                    </CollapsibleComponent>
                 
-            </div>
-        );
+                </div>
+            );}
+        else {
+            return(
+                <div>
+                    <div class="w-full flex space-x-2">
+                        <h1 class="w-10/12 text-scarlet m-5 text-4xl">{this.state.student.last_name}, {this.state.student.first_name}</h1>
+                        <Link class="bg-scarlet text-white py-2 px-6 rounded-3xl h-1/2 mt-5" to={{pathname: "/update", student:this.state.student}}>Update Milestones</Link>
+                    </div>
+                    <div id="progressBar" >
+                        <div id="bar" style={{width: setWidth}}></div>
+                    </div>
+                    <div>
+                        <h2 class="pt-5 w-10/12 text-black m-5 text-4xl">
+                            Milestones
+                        </h2>
+                    </div>
+                    <CollapsibleComponent class="m-5  p-5 text-black font-bold text-center text-lg bg-gray-400 bg-opacity-30 grid grid-cols-4 gap-5">
+                        {sortedMilestones.map((milestone) => {
+                            let color = ""
+                            if(milestone.status === 0) {
+                                color = "bg-red-600";
+                            } else if(milestone.status === 1) {
+                                color = "bg-yellow-500";
+                            } else if(milestone.status === 2) {
+                                color = "bg-gray-500";
+                            } else if(milestone.status === 3) {
+                                color = "bg-green-500";
+                            }
+
+                            return (
+                                <div>
+                                    <CollapsibleHead class="pb-5 pl-5 bg-white rounded-lg">
+                                        <div className={`my-3.5 circle ${color} align-middle float-left`}></div>
+                                        <div class="w-10/12 float-right">{milestone.name}</div>
+                                    </CollapsibleHead>
+                                    <CollapsibleContent class="bg-gray-400 bg-opacity-30">
+                                        <p>{milestone.description}</p>
+                                    </CollapsibleContent>
+                                </div>
+                            )
+                        })}
+                    </CollapsibleComponent>
+                    <div>
+                    <AdvisorNotesComponent student = {this.state.student} >
+                    </AdvisorNotesComponent>
+                    </div>
+                </div>
+            );
+        }
+        
     }
 }
