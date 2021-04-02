cc5e69ca9b56775777de37d5cec2953b047ea654
jloudenslager <loudenslager.15@osu.edu>
2021-04-02 18:28:46 -0400

    Format date using Moment instead

--- client/src/ReviewContainer.js
+++ client/src/ReviewContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Moment from 'moment';
 
 export default class ReviewContainer extends Component {
     constructor(props) {
@@ -51,13 +52,11 @@ export default class ReviewContainer extends Component {
         return(
             <section className="flex flex-col items-center overflow-y-auto">
                 {this.state.reviews.map((r) => {
-                    const d = new Date(Date.parse(r.submitted));
-                    const date = `${d.getMonth()+1}/${d.getDate()}/${d.getFullYear()}`;
                     return(
                         <div className="flex flex-col items-center w-11/12 text-center mb-2 bg-white rounded-lg border border-yellow">
                             <h3 className="text-scarlet">{r.first_name} {r.last_name}</h3>
                             <h4 className="text-gray-400">{r.name}</h4>
-                            <h4 className="text-gray-400">Submitted: {date}</h4>
+                            <h4 className="text-gray-400">Submitted: {Moment(r.submitted).format('MM/DD/YY')}</h4>
                             <div className="w-full border border-b-1 mb-1"></div>
                             <button className="w-11/12 bg-red-700 text-white mb-2 py-1 rounded-md" onClick={() => this.updateMilestone(r, 0)}>Decline</button>
                             <button className="w-11/12 bg-green-600 text-white mb-2 py-1 rounded-md" onClick={() => this.updateMilestone(r, 3)}>Approve</button>
